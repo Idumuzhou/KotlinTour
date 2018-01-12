f02f3d45e0c8b002707978093e80fb5bb9734042
Luoxuliang <962526394@qq.com>
2018-01-12 17:04:01 +0800

    update

---
+++ app/src/main/java/com/kotlintour/ui/TestActivity.kt
@@ -0,0 +1,11 @@
+package com.kotlintour.ui
+
+/**
+ * @priject KotlinTour
+ * @author LuoXuLiang
+ * @description:  description 
+ * @date: 2018/1/12 下午5:03
+ */
+class TestActivity {
+    //Test
+}
